7cb47287c409176e2984d07ba584f0679bc3c384
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-01-15 07:14:46 +0600

    Add category creation functionality

--- components/shared/AddNewCategory.tsx
+++ components/shared/AddNewCategory.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogClose,
@@ -8,11 +10,20 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { useState } from "react";
 import { HiMiniPlus } from "react-icons/hi2";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
 export default function AddNewCategory() {
+  const [category, setCategory] = useState("");
+
+  function handleCreate() {
+    if (!category) return toast.error("Please enter a category name");
+    toast.success("Category created successfully");
+  }
+
   return (
     <Dialog>
       <DialogTrigger className="inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
@@ -27,7 +38,9 @@ export default function AddNewCategory() {
         </DialogDescription>
         <DialogFooter>
           <DialogClose asChild>
-            <Button type="submit">Add</Button>
+            <Button onClick={handleCreate} type="submit">
+              Add
+            </Button>
           </DialogClose>
         </DialogFooter>
       </DialogContent>
